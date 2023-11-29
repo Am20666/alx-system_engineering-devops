62cf1300ff08dd8c1172962fff6f967983f85c70
Amin dr <dr.amin.std@gmail.com>
2023-11-29 18:54:16 +0100

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,5 @@
-#!/usr/bin/pup
-#installs a package - flask
-
-package { 'flask':
+# Install a package from pip3 - flask version 2.1.0
+package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
-
